67f943f2d64805c4d9ab675932c902da2505f213
Albin Bååw <albin.baaw@gmail.com>
2022-04-03 11:49:52 +0200

    fix: make calc_diameter optional

--- lib/util.py
+++ lib/util.py
@@ -14,7 +14,7 @@ def plot(title, x, y, x_label="", y_label="", save_file=False):
     plt.savefig(title + ".png")
   plt.show()
 
-def describe_graph(data, log_scale = False):
+def describe_graph(data, log_scale = False, calc_diameter=False):
   graph = to_networkx(data)
 
   is_strongly_connected = nx.is_strongly_connected(graph)
@@ -22,7 +22,7 @@ def describe_graph(data, log_scale = False):
   print("graph is weakly connected component: {}".format(nx.is_weakly_connected(graph)))
   print("graph has # weakly connected components: {}".format(nx.number_weakly_connected_components(graph)))
   print("graph has # isolated nodes: {}".format(nx.algorithms.number_of_isolates(graph)))
-  if is_strongly_connected:
+  if is_strongly_connected and calc_diameter:
     print("graph has diameter: {}".format(nx.algorithms.distance_measures.diameter(graph)))
 
   degree_histogram = nx.degree_histogram(graph)
