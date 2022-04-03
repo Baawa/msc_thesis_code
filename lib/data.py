69f8c3b616a7b18909fee07dde37d179220ec202
Albin Bååw <albin.baaw@gmail.com>
2022-04-03 18:21:15 +0200

    fix: splitting of datasets that resulted in skewed calibration set distribution + rerun experiments

--- lib/data.py
+++ lib/data.py
@@ -2,7 +2,7 @@ import torch
 from torch_geometric.data import Data
 import numpy as np
 
-def split_dataset(data, test_size = 0.2, calibration_size = 0.1):
+def split_dataset(data, test_frac = 0.2, calibration_frac = 0.1):
   """
   Splits dataset into training, calibration, and test set.
   Training set is disjoint from the calibration and test set. This is how they do it in the GraphSAGE experiments: https://github.com/williamleif/GraphSAGE/blob/a0fdef95dca7b456dab01cb35034717c8b6dd017/graphsage/minibatch.py#L76
@@ -32,18 +32,14 @@ def split_dataset(data, test_size = 0.2, calibration_size = 0.1):
 
 
   # split into training and test
-  test_indices = np.random.choice(data.num_nodes, int(data.num_nodes * test_size), replace=False)
+  test_cal_indices = np.random.choice(data.num_nodes, int(data.num_nodes * (test_frac+calibration_frac)), replace=False)
+  
+  test_size = int(test_frac * data.num_nodes)
+  test_indices = test_cal_indices[:test_size]
+  calibration_indices = test_cal_indices[test_size:]
 
   train_indices = np.arange(data.num_nodes, dtype=np.int64)
-  train_indices = np.delete(train_indices, test_indices)
+  train_indices = np.delete(train_indices, test_cal_indices)
   train = split(data, train_indices)
 
-  # split into proper train set and calibration set
-  calibration_indices = np.random.choice(train.num_nodes, int(train.num_nodes * calibration_size), replace=False)
-  
-  # get proper train set
-  proper_train_indices = np.arange(train.num_nodes, dtype=np.int64)
-  proper_train_indices = np.delete(proper_train_indices, calibration_indices)
-  proper_train = split(train, proper_train_indices)
-
-  return proper_train, calibration_indices.tolist(), test_indices.tolist()
+  return train, calibration_indices.tolist(), test_indices.tolist()
