d679474d72af0a52aa2cd77d27d925460c8e6f4c
Albin Bååw <albin.baaw@gmail.com>
2022-04-13 22:17:59 +0200

    Speed up data splitting (1780x faster)

--- lib/data.py
+++ lib/data.py
@@ -8,28 +8,6 @@ def split_dataset(data, test_frac = 0.2, calibration_frac = 0.1):
   Training set is disjoint from the calibration and test set. This is how they do it in the GraphSAGE experiments: https://github.com/williamleif/GraphSAGE/blob/a0fdef95dca7b456dab01cb35034717c8b6dd017/graphsage/minibatch.py#L76
   returns Data, list[int], list[int]
   """
-  def split(data, indices):
-    x = data.x[indices]
-    edge_index = None
-    
-    indices_dict = dict(zip(indices, np.arange(len(indices))))
-    
-    for edge in torch.t(data.edge_index):
-      if edge[0].item() not in indices or edge[1].item() not in indices:
-        continue
-
-      new_edge = torch.LongTensor([[indices_dict[edge[0].item()]],[indices_dict[edge[1].item()]]])
-
-      if edge_index is None:
-        edge_index = new_edge
-      else:
-        edge_index = torch.cat((edge_index, new_edge), 1)
-    # END: for
-    
-    y = data.y[indices]
-
-    return Data(x=x, edge_index=edge_index, y=y)
-
 
   # split into training and test
   test_cal_indices = np.random.choice(data.num_nodes, int(data.num_nodes * (test_frac+calibration_frac)), replace=False)
@@ -43,3 +21,21 @@ def split_dataset(data, test_frac = 0.2, calibration_frac = 0.1):
   train = split(data, train_indices)
 
   return train, calibration_indices.tolist(), test_indices.tolist()
+
+def split(data: Data, indices):
+    x = data.x[indices]
+
+    edge_index_0 = np.isin(data.edge_index[0].tolist(), indices)
+    edge_index_1 = np.isin(data.edge_index[1].tolist(), indices)
+
+    indices_to_keep = np.logical_and(edge_index_0, edge_index_1)
+
+    edge_index = data.edge_index[:,indices_to_keep]
+    
+    indices_dict = dict(zip(indices, np.arange(len(indices))))
+    edge_index[0] = edge_index[0].apply_(lambda x: indices_dict[x])
+    edge_index[1] = edge_index[1].apply_(lambda x: indices_dict[x])
+    
+    y = data.y[indices]
+
+    return Data(x=x, edge_index=edge_index, y=y)
