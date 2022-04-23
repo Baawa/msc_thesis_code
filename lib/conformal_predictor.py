758774fa519998113fc91de9080af1fffe8ed100
Albin Bååw <albin.baaw@gmail.com>
2022-04-23 17:15:20 +0200

    feat: split out cp and graphsage settings and turn into their own classes

--- lib/conformal_predictor.py
+++ lib/conformal_predictor.py
@@ -1,27 +1,188 @@
 import torch
 
-class ConformalClassifier():
+
+def get_nonconformity_measure_for_classification(y_proba: torch.Tensor, version="v2"):
+    """Returns nonconformity measure for all possible classes"""
+    if version == "v1":  # non-conformity measure according to "Reliable diagnosis of acute abdominal pain with conformal prediction" (cp_in_medicine)
+        alphas = []
+        for i in range(y_proba.shape[0]):
+            y_probas_for_false_classes = torch.cat(
+                [y_proba[0:i], y_proba[i+1:]])
+            max_prob_not_y = torch.max(y_probas_for_false_classes)
+            a = max_prob_not_y - y_proba[i].item()
+            alphas.append(a)
+        alphas = torch.tensor(alphas)
+    elif version == "v2":
+        alphas = 1 - y_proba
+    else:
+        raise ValueError("Invalid version")
+
+    return alphas
+
+
+class InductiveConformalClassifier():
+    """
+    Inductive conformal prediction based on "Tutorial On Conformal Prediction" by Shafer & Vovk (p. 388).
+    """
+
+    def __init__(self, alphas, y):
+        """
+        alphas: non-conformity measures (n_samples).
+        y: targets (n_samples)
+        """
+        super()
+
+        self.alphas = alphas
+        self.y = y
+
+        return
+
+    def predict(self, alphas, confidence_level):
+        """
+        Retrieve a prediction region for the provided nonconformity measures 
+        \nOBS! Only single samples allowed
+        \nconfidence_level: e.g. 0.99
+        """
+
+        significance_level = 1 - confidence_level
+
+        prediction_region = []
+
+        classes = torch.unique(self.y)
+
+        for y in classes:
+            ai = alphas[y]
+
+            # non-conformity scores
+            a = self.alphas
+
+            # calculate p-score
+            c = torch.count_nonzero(a >= ai)
+            p_score = c / len(a)
+
+            if p_score > significance_level:
+                prediction_region.append(y)
+        # END: for
+
+        return prediction_region
+
+class MondrianConformalClassifier():
+    """
+    Extended with Mondrian Conformal Prediction option based on "Mondrian Conformal Regressors" by Boström & Johansson, and https://gist.github.com/dsleo/2880882b5e1c1feab677c4cf421e806d
+    """
+
+    def __init__(self, alphas, y):
+        """
+        alphas: non-conformity measures (n_samples).
+        y: targets (n_samples)
+        """
+        super()
+
+        self.alphas = alphas
+        self.y = y
+
+        return
+
+    def predict(self, alphas, confidence_level):
+        """
+        Retrieve a prediction region for the provided nonconformity measures 
+        \nOBS! Only single samples allowed
+        \nconfidence_level: e.g. 0.99
+        """
+
+        significance_level = 1 - confidence_level
+
+        prediction_region = []
+
+        classes = torch.unique(self.y)
+
+        _alphas = self.alphas[self.y == y]
+
+        for y in classes:
+            ai = alphas[y]
+
+            # non-conformity scores
+            a = _alphas
+
+            # calculate p-score
+            c = torch.count_nonzero(a >= ai)
+            p_score = c / len(a)
+
+            if p_score > significance_level:
+                prediction_region.append(y)
+        # END: for
+
+        return prediction_region
+
+class NodeDegreeMondrianConformalClassifier():
+    """
+    Novel MCP using the node degree instead of the class.
+    """
+
+    def __init__(self, alphas, y, node_degrees):
+        """
+        alphas: non-conformity measures (n_samples).
+        y: targets (n_samples)
+        node_degrees: the samples node degrees (n_samples)
+        """
+        super()
+
+        self.alphas = alphas
+        self.y = y
+        self.node_degrees = node_degrees
+
+        return
+
+    def predict(self, alphas, confidence_level, node_degree):
+        """
+        Retrieve a prediction region for the provided nonconformity measures 
+        \nOBS! Only single samples allowed
+        \nconfidence_level: e.g. 0.99
+        """
+
+        significance_level = 1 - confidence_level
+
+        prediction_region = []
+
+        classes = torch.unique(self.y)
+
+        _alphas = self.alphas[self.node_degrees == node_degree]
+
+        for y in classes:
+            ai = alphas[y]
+
+            # non-conformity scores
+            a = _alphas
+
+            # calculate p-score
+            c = torch.count_nonzero(a >= ai)
+            p_score = c / len(a)
+
+            if p_score > significance_level:
+                prediction_region.append(y)
+        # END: for
+
+        return prediction_region
+
+class NodeDegreeWeightedConformalClassifier():
   """
-  Inductive conformal prediction based on "Tutorial On Conformal Prediction" by Shafer & Vovk (p. 388).
-  Extended with Mondrian Conformal Prediction option based on "Mondrian Conformal Regressors" by Boström & Johansson, and https://gist.github.com/dsleo/2880882b5e1c1feab677c4cf421e806d
+  Inspired by "Conformal prediction beyond exchangeability" (2022)
   """
-  def __init__(self, alphas, y, mondrian=False, mondrian_taxonomy=None, cumulative_taxonomy=False):
+  def __init__(self, alphas, y, node_degrees):
     """
     alphas: non-conformity measures (n_samples).
     y: targets (n_samples)
-    mondrian_taxonomy: the samples categories (n_samples)
+    node_degrees: the samples node_degrees (n_samples)
     """
     super()
 
     self.alphas = alphas
     self.y = y
-    self.mondrian = mondrian
-    self.mondrian_taxonomy = mondrian_taxonomy 
-    self.cumulative_taxonomy = cumulative_taxonomy
+    self.node_degrees = node_degrees
 
     return
   
-  def predict(self, alphas, confidence_level, mondrian_category=None):
+  def predict(self, alphas, node_degree, confidence_level):
     """
     Retrieve a prediction region for the provided nonconformity measures 
     \nOBS! Only single samples allowed
@@ -33,26 +194,21 @@ class ConformalClassifier():
     prediction_region = []
 
     classes = torch.unique(self.y)
+    
+    # weights
+    max_degree = torch.max(torch.cat((self.node_degrees, torch.tensor([node_degree]))))
+    normalized_degrees = self.node_degrees / max_degree
+    weights = 1 - torch.abs((node_degree/max_degree) - normalized_degrees)
+    sum_weights = torch.sum(weights) + 1
+    
+    cal_normalized_weights = weights / sum_weights
+    sample_normalized_weight = 1 / sum_weights
 
     for y in classes:
-      ai = alphas[y]
+      ai = alphas[y] * sample_normalized_weight
 
       # non-conformity scores
-      if self.mondrian:
-        if mondrian_category is not None:
-          if self.mondrian_taxonomy is None:
-            raise ValueError("Expected self.mondrian_taxonomy, but found None")
-          
-          if self.cumulative_taxonomy:
-            a = self.alphas[torch.logical_and(self.mondrian_taxonomy <= mondrian_category, self.y == y)]
-          else:
-            a = self.alphas[torch.logical_and(self.mondrian_taxonomy == mondrian_category, self.y == y)]
-        
-        else: # class-based mondrian
-          a = self.alphas[self.y == y]
-      
-      else: # not mondrian
-        a = self.alphas
+      a = self.alphas * cal_normalized_weights
       
       # calculate p-score
       c = torch.count_nonzero(a >= ai)
@@ -64,19 +220,70 @@ class ConformalClassifier():
       
     return prediction_region
 
-def get_nonconformity_measure_for_classification(y_proba: torch.Tensor, version="v1"):
-  """Returns nonconformity measure for all possible classes"""
-  if version == "v1": # non-conformity measure according to "Reliable diagnosis of acute abdominal pain with conformal prediction" (cp_in_medicine)
-    alphas = []
-    for i in range(y_proba.shape[0]):
-      y_probas_for_false_classes = torch.cat([y_proba[0:i], y_proba[i+1:]])
-      max_prob_not_y = torch.max(y_probas_for_false_classes)
-      a = max_prob_not_y - y_proba[i].item()
-      alphas.append(a)
-    alphas = torch.tensor(alphas)
-  elif version == "v2":
-    alphas = 1 - y_proba
-  else:
-    raise ValueError("Invalid version")
-    
-  return alphas
+class LegacyConformalClassifier():
+    """
+    Inductive conformal prediction based on "Tutorial On Conformal Prediction" by Shafer & Vovk (p. 388).
+    Extended with Mondrian Conformal Prediction option based on "Mondrian Conformal Regressors" by Boström & Johansson, and https://gist.github.com/dsleo/2880882b5e1c1feab677c4cf421e806d
+    """
+
+    def __init__(self, alphas, y, mondrian=False, mondrian_taxonomy=None, cumulative_taxonomy=False):
+        """
+        alphas: non-conformity measures (n_samples).
+        y: targets (n_samples)
+        mondrian_taxonomy: the samples categories (n_samples)
+        """
+        super()
+
+        self.alphas = alphas
+        self.y = y
+        self.mondrian = mondrian
+        self.mondrian_taxonomy = mondrian_taxonomy
+        self.cumulative_taxonomy = cumulative_taxonomy
+
+        return
+
+    def predict(self, alphas, confidence_level, mondrian_category=None):
+        """
+        Retrieve a prediction region for the provided nonconformity measures 
+        \nOBS! Only single samples allowed
+        \nconfidence_level: e.g. 0.99
+        """
+
+        significance_level = 1 - confidence_level
+
+        prediction_region = []
+
+        classes = torch.unique(self.y)
+
+        for y in classes:
+            ai = alphas[y]
+
+            # non-conformity scores
+            if self.mondrian:
+                if mondrian_category is not None:
+                    if self.mondrian_taxonomy is None:
+                        raise ValueError(
+                            "Expected self.mondrian_taxonomy, but found None")
+
+                    if self.cumulative_taxonomy:
+                        a = self.alphas[torch.logical_and(
+                            self.mondrian_taxonomy <= mondrian_category, self.y == y)]
+                    else:
+                        a = self.alphas[torch.logical_and(
+                            self.mondrian_taxonomy == mondrian_category, self.y == y)]
+
+                else:  # class-based mondrian
+                    a = self.alphas[self.y == y]
+
+            else:  # not mondrian
+                a = self.alphas
+
+            # calculate p-score
+            c = torch.count_nonzero(a >= ai)
+            p_score = c / len(a)
+
+            if p_score > significance_level:
+                prediction_region.append(y)
+        # END: for
+
+        return prediction_region
--- lib/graphsage.py
+++ lib/graphsage.py
@@ -2,12 +2,12 @@ import torch
 from torch_geometric.nn import SAGEConv
 from torch_geometric.loader import NeighborLoader
 
-class GraphSAGE(torch.nn.Module):
+class GraphSAGEWithSampling(torch.nn.Module):
   def __init__(self, input_dim, hidden_dim, output_dim, num_layers, sampling_size):
     """
       sampling_size: number of neighbors to sample at each layer (list[int]).
     """
-    super(GraphSAGE, self).__init__()
+    super(GraphSAGEWithSampling, self).__init__()
 
     self.num_layers = num_layers
 
@@ -77,3 +77,73 @@ class GraphSAGE(torch.nn.Module):
       y_hat.extend(out.tolist())
     
     return torch.tensor(y_hat)
+
+class GraphSAGE(torch.nn.Module):
+  def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+    """
+      sampling_size: number of neighbors to sample at each layer (list[int]).
+    """
+    super(GraphSAGE, self).__init__()
+
+    self.num_layers = num_layers
+
+    self.convs = torch.nn.ModuleList()
+    self.bns = torch.nn.ModuleList()
+
+    # input layer
+    self.convs.append(SAGEConv(in_channels=input_dim, out_channels=hidden_dim, aggr="mean"))
+    self.bns.append(torch.nn.BatchNorm1d(hidden_dim))
+
+    # hidden layers
+    for _ in range(0, num_layers-1):
+      self.convs.append(SAGEConv(in_channels=hidden_dim, out_channels=hidden_dim, aggr="mean"))
+      self.bns.append(torch.nn.BatchNorm1d(hidden_dim))
+
+    # output layer
+    self.convs.append(SAGEConv(in_channels=hidden_dim, out_channels=output_dim, aggr="mean"))
+
+    self.softmax = torch.nn.LogSoftmax(dim=1)
+
+    self.dropout = 0.1
+
+  def reset_parameters(self):
+    for conv in self.convs:
+        conv.reset_parameters()
+
+  def forward(self, x, adj_t):
+    embedding = x
+    for i, conv in enumerate(self.convs[:-1]):
+      embedding = conv(x=embedding, edge_index=adj_t)
+      embedding = self.bns[i](embedding)
+      embedding = torch.nn.functional.relu(embedding)
+      embedding = torch.nn.functional.dropout(embedding, p=self.dropout, training=self.training)
+    
+    # output layer
+    embedding = self.convs[-1](x=embedding, edge_index=adj_t)
+
+    node_class = self.softmax(embedding)
+
+    return node_class
+
+  def train_model(self, data, optimizer, loss_fn, batch_size):
+    self.train()
+
+    optimizer.zero_grad()
+    
+    out = self(data.x, data.edge_index)
+
+    y_true = data.y.reshape(-1)
+
+    loss = loss_fn(out, y_true)
+
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+  
+  def predict(self, data):
+    self.eval()
+    
+    out = self(data.x, data.edge_index)
+    
+    return torch.tensor(out)
